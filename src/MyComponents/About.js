af9fec0ace46a0597b954e2c65fabf6752f2be7c
Mohit Kumar <vegasgangstar25@gmail.com>
2022-07-10 19:07:37 +0530

    updated about

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-        <p>This is a simple todo list react app.</p>
+        <p className='text-center'>This is a simple todo list react app.</p>
     </div>
   )
 }
